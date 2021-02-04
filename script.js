26be643afb1a7cce7c9a1b6a432ec8d10b43345e
clau9156 <clau9156@stud.kea.dk>
2021-02-04 17:43:47 +0100

    rough structure

---
+++ script.js
@@ -0,0 +1,53 @@
+// start
+start ();
+
+function start() {
+    console.log("start");
+
+    playerChoose();
+}
+
+// player choose
+function playerChoose() {
+    console.log("playerChoose");
+
+    computerRandomChoice();
+}
+
+// computer random pick
+function computerRandomChoice() {
+    console.log("computerRandomChoice");
+
+    showFinalChoice();
+}
+
+// show final choice
+function showFinalChoice() {
+    console.log("showFinalChoice");
+
+    determineWinner();
+} 
+
+// determine winner
+function determineWinner() {
+    console.log("determineWinner");
+
+}
+
+// show win
+function showWin() {
+    console.log("showWin");
+
+}
+
+// show lose
+function showLose() {
+    console.log("showLose");
+
+}
+
+// show draw
+function showDraw() {
+    console.log("showDraw");
+
+}
