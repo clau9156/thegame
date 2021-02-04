1666e1ebd6e24603e6f4c85fa021cefe87650c4f
clau9156 <clau9156@stud.kea.dk>
2021-02-04 20:11:24 +0100

    let and add event listener in player choose and random in computer

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
+// let
+// let win = 0;
+// let lose = 0;
+// let draw = 0;
+let rock = 0;
+let paper = 1;
+let scissor = 2;
+
+let rockButton = document.querySelector("button.rock");
+let paperButton = document.querySelector("button.paper");
+let scissorButton = document.querySelector("button.scissor");
+let player = document.querySelector("#player1");
+let computer = document.querySelector("#player2");
+
 // start
 start ();
+// window.addEventListener("load", start);
 
 function start() {
     console.log("start");
+    //animation closed hand 3 times
+    //or later
 
     playerChoose();
 }
@@ -10,13 +27,28 @@ function start() {
 // player choose
 function playerChoose() {
     console.log("playerChoose");
-
+    rockButton.addEventListener("click", );
+    paperButton.addEventListener("click", );
+    scissorButton.addEventListener("click", );
     computerRandomChoice();
 }
 
 // computer random pick
 function computerRandomChoice() {
     console.log("computerRandomChoice");
+    let rndNumber = Math.floor(Math.random()*3);
+    if (rndNumber==0) {
+        console.log("rock");
+
+    }
+    if (rndNumber==1) {
+        console.log("paper");
+
+    }
+    if (rndNumber==2) {
+        console.log("scissor");
+
+    }
 
     showFinalChoice();
 }
@@ -24,7 +56,6 @@ function computerRandomChoice() {
 // show final choice
 function showFinalChoice() {
     console.log("showFinalChoice");
-
     determineWinner();
 } 
 
